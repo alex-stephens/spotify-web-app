9c704e7354516a219ec3fe6ab6d0b0a33a7f56e7
Alex Stephens <alexstephens9@gmail.com>
2018-12-17 16:28:04 +1100

    able to generate lists of top tracks with different query parameters + did some refactoring

--- app/public/scripts/playlists.js
+++ app/public/scripts/playlists.js
@@ -1,15 +1,49 @@
-var title = "Your Playlists"
+
+const subtitles = new Map([['short_term', 'Short Term (4 weeks)'],
+                        ['medium_term', 'Medium Term (6 Months)'],
+                        ['long_term', 'Long Term (Years)']]);
 
 function songTemplate(song) {
     return  `
-      <b>${song.name}</b> <span class="artist">${song.artists[0].name}</span><br>
+      <li><b>${song.name}</b> <span class="artist">${song.artists[0].name}</span><br></li>
     `;
 }
 
-function buildPlaylistTemplate(response) {
-    var string = JSON.stringify(response);
+/* Time is 'short_term', 'medium_term' or 'long_term'.
+ * Limit is <= 50.
+ */
+function runQuery(access_token, time, limit) {
+
+    /* https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=30&offset=0 */
+    params = 'https://api.spotify.com/v1/me/top/tracks?time_range=' +
+           time + '&limit=' + limit + '&offset=0';
+
+    // playlist generator query
+    $.ajax({
+        url: params,
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+            buildPlaylist(response, time);
+        }
+    });
+
+}
+
+function buildPlaylist(response, time) {
+
+    var title = "Top Tracks : ";
+
+    if (!subtitles.has(time)){
+        return; /* TODO: handle error */
+    }
+
+    title += subtitles.get(time);
+
     out = `
-        <h1 id="playlists-title">${title}</h1>
+        <h2 id="playlists-title">${title}</h2>
+        <ol>
     `;
 
     for (var i = 0; i < response["items"].length; i++){
@@ -17,5 +51,8 @@ function buildPlaylistTemplate(response) {
         out += songTemplate(k);
     }
 
-    document.getElementById('playlists').innerHTML = out;
+    out += `</ol>`
+
+
+    document.getElementById('playlists').innerHTML += out;
 }
